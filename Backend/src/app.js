03e31ddfc17da8881a0c40b74bbf04141389aa7a
Siddhesh-2005 <siddheshbagde123@gmail.com>
2025-11-29 16:48:02 +0530

    Add utils

---
+++ Backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+//middlewares
+
+//allowing frontend from another origin to communicate 
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//informing server that we are accepting data in json  
+app.use(express.json({
+    limit:"16kb"
+}))
+
+//accepting data through url
+app.use(express.urlencoded({
+    extended:true,//extended means nested object
+    limit:"16kb",             
+}))
+
+//allows to access the public files like img,favicon,etc
+app.use(express.static("public"))
+
+//to perform crud ops on cookies present on user's browser
+app.use(cookieParser())
+
+
+
+export {app}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,20 @@
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-
+//the env is loaded first
 dotenv.config({
-    path:"./.env"
+    path: "./.env",
+});
 
-})
 
+//when the index.js starts it first connects the db 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB connection FAILED....", err);
+    });
